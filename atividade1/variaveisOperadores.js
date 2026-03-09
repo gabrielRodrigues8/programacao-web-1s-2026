1c6dffe47fddf61d4dff5ba68a04516df0998b1a
gabrielRodrigues8 <gabriel.biliu@a.ucb.br>
2026-03-09 20:46:26 -0300

    Entrega Atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,11 @@
+let num_A = 15;
+let num_B = 10;
+
+let resultadoSoma = num_A + num_B;
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${resultadoSoma}`);
+let resultadoSubtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${resultadoSubtracao}`);
+let resultadoMultiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${resultadoMultiplicacao}`);
+let resultadoDivisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${resultadoDivisao}`);
